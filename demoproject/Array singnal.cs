5f4383911ab0dd97853edb71a0ca41f828454a0a
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-09-22 17:21:13 +0530

    add array

---
+++ demoproject/Array singnal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    internal class Array_singnal
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 5, 6, 6, 7, 7, };
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
