8c2ff7b955f1e2fc5b1b98c8ec1a78e1bbd8bd63
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-09-22 19:27:39 +0530

    add file

---
+++ demoproject/Addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    internal class Addition
+    {
+        static void Main()
+        {
+            int a = 10;
+            int b = 10;
+            int c;
+            c = a + b;
+            Console.WriteLine("addition is"+(a + b));
+
+            Console.WriteLine("subtraction is"+(a-b));
+        }
+    }
+}
