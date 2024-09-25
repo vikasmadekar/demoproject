5ee4c1e0cd65be90528a0f219424da1159038a88
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-09-25 18:38:36 +0530

    add class

---
+++ demoproject/Animal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    class Circle
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing the circle");
+        }
+
+
+
+    }
+    class Rectangle : Circle
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing the Rectangle");
+
+        }
+    }
+
+    internal class Animal
+    {
+        static void Main()
+        {
+            Circle c1 = new Circle();
+
+            Rectangle rect = new Rectangle();
+
+            c1.Draw();
+            rect.Draw();
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
+    
+
+   
+
---
+++ demoproject/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    class Staticpolymorphism
+    {
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public float Sub(float a, float b)
+        {
+            return a - b;
+
+        }
+    }
+
+    internal class Calculator
+    {
+        static void Main()
+        {
+            Staticpolymorphism sta = new Staticpolymorphism();
+
+            int sub1 = sta.Sub(4, 1);
+            float sub2 = sta.Sub(55.5F, 36.5F);
+
+            Console.WriteLine("the subtraction is:" + sub1);
+            Console.WriteLine("the subtraction is:" + sub2);
+        }
+    }
+}
+
+
