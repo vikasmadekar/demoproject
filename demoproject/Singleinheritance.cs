38afe4b239c89fe4f5cbb1d17dedca6826031596
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-09-27 15:05:01 +0530

    add a class

---
+++ demoproject/Singleinheritance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace demoproject
+{   class Persion
+
+    {
+        
+        public string name { get;set; } 
+        public int age { get;set; }
+
+    }
+    class Student : Persion
+    {
+        public void Show()
+        {
+            Console.WriteLine($"persion name:={ name}");
+            Console.WriteLine($"persion age:={ age}");
+        }
+    }
+    internal class Singleinheritance
+    {
+        static void Main()
+        {
+             
+            Student std = new Student();
+            {
+                std.name = "vikas";
+                std.age = 16;
+
+            };
+
+            std.Show();
+        }
+    }
+}
