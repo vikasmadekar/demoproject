2d19ec20969cb6527ca859fbc3a809e6bba0b072
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-09-27 16:56:10 +0530

    Add Inheritance

---
+++ demoproject/Multilevel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    class Person // base class
+    {
+        
+       
+        public string Name { get; set; }
+        
+       
+           
+    }
+
+       
+    class Teacher : Persion //Person cha  child class
+    {
+        public int id { get; set; }
+        public float Salary { get; set; }
+    }
+    class Principle :Teacher// teacher cha child class
+    {
+        public void Display()
+        {
+            Console.WriteLine($"The person id:={id}");
+            Console.WriteLine($"The name persion:{name}");
+            
+            Console.WriteLine($"The employee salary:{Salary }");
+
+        }
+         
+
+    }
+    internal class Multilevel
+    {
+
+        static void Main()
+        {
+            Principle principle = new Principle();
+            
+                principle.name = "vikas";
+                principle.id = 1;
+                principle.Salary = 500000.5F;
+
+
+            principle.Display();
+              
+            
+        }
+    }
+}
