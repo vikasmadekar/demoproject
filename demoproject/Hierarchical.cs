965415087cff260786cc67e92fab3bdd43fe4864
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-09-27 16:54:47 +0530

    Add Inheritance

---
+++ demoproject/Hierarchical.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+   public   class Shape5
+    {
+      public   string  color;
+            public void DisplayDetails()
+        {
+            Console.WriteLine(color);
+        }
+    }
+   public  class Circle3:Shape5
+    {
+       public  Circle3 (string color)
+        {
+            this.color = color;
+        }
+
+    }
+    public class Rectangle4:Shape5
+    {
+       public   Rectangle4 (String color)
+        {
+            this.color = color;
+        }
+    }
+    internal class Hierarchical
+    {
+        static void Main()
+        {
+            Circle3 circle =new Circle3("Yellow");
+            Console.Write("Circle color");
+            circle .DisplayDetails();
+            Rectangle4  rec=new Rectangle4("Black");
+            Console.Write("Rectangle color ");
+            rec.DisplayDetails();
+
+            
+        }
+            
+    }
+}
---
+++ demoproject/Multidimesional.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    internal class Multidimesional
+    {
+        static void Main()
+        {
+            int[,] arr= new int[3, 4]
+            {
+                 { 11 ,23 ,44 ,55 },
+
+               { 12 ,23, 31, 66 },
+
+                { 55 ,66 ,77 ,88  }
+
+
+              };
+
+            //for (int i = 0; i < arr.GetLength(0);i++)
+            //{
+              //  for(int j=0;j<arr.GetLength(1);j++)
+                //{
+                  //  Console.WriteLine(arr[i,j]);
+               // }
+                
+           // }
+           foreach (int i in arr)
+            {
+
+                Console.WriteLine(i);
+
+            }
+
+        }
+    }
+}
