d836152c9d3acc098e235a3d5f77da9d4f791355
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-10-01 13:37:06 +0530

    add a class

---
+++ demoproject/IPlable.cs
@@ -0,0 +1,40 @@
+﻿using demoproject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    internal interface IPlable
+    {
+        public void Play();
+    }
+    class Videoplayer : IPlable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing the video");
+        }
+    }
+    class  MusicPlayer:IPlable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing the Music");
+        }
+    }
+    class Multiple
+    {
+        static void Main()
+        {
+            Videoplayer v=new Videoplayer();
+            v.Play();   
+            MusicPlayer m=new MusicPlayer();
+            m.Play();   
+        }
+    }
+}
+ 
