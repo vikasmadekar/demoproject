004329aeb42ac62e8b2f44e709807dcc42f91753
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-10-02 15:35:48 +0530

    Hashtable a program

---
+++ demoproject/VmHashTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    internal class VmHashTable
+    {
+        static void Main()
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("name", "vikas");
+            hashtable.Add("id", 20);
+            hashtable.Add("phone no", 123456789);
+            hashtable.Add("salary", 123);
+            Console.WriteLine("Display the element");
+             
+            foreach (var item in hashtable)
+            {
+                Console.WriteLine(item );
+            }
+            Console.ReadLine ();
+            hashtable.Remove("id");
+            Console.WriteLine("element the remove");
+            foreach (var item in hashtable)
+            {
+                Console.WriteLine(item );
+            }
+        }
+    }
+}
