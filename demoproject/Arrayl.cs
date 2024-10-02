01e8d4a5f76651b17249c1cbe129b02baa192163
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-10-02 14:13:39 +0530

    Add a class

---
+++ demoproject/Arrayl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    internal class Arrayl
+    {
+        static void Main()
+        {
+           ArrayList list = new ArrayList();
+
+            list.Add(12);
+            list.Add ("hello");
+            list.Add (11.54);
+            Console.WriteLine("Display the array list element");
+
+            foreach(var item in list)
+            {
+                Console.WriteLine(item );
+            }
+            Console.ReadLine();
+
+            list .Reverse();
+            Console.WriteLine("Reverse array list");
+            foreach (var item in list)
+            {
+                Console.WriteLine (item );
+            }
+            Console.ReadLine();
+            list.RemoveAt (0);
+            Console.WriteLine("Remove the element");
+            foreach ( var item in list)
+            {
+                Console.WriteLine(item );
+            }
+            Console.ReadLine();
+            list.Add(232);
+            list.Add("madekar");
+            Console.WriteLine("Add the element");
+            foreach (var item in list )
+            {
+                Console.WriteLine(item );   
+            }
+            Console.ReadLine();     
+            list.Clear();
+            Console.WriteLine("clear the element");
+            foreach (var item in list )
+            {
+                Console.WriteLine("clear the element");
+            }
+          Console.ReadLine ();
+
+        }
+    }
+}
