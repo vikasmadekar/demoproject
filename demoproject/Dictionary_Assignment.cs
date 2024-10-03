ebadaa002ba284b006725c7663392dae5617a896
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-10-03 13:05:09 +0530

    Add dictionary

---
+++ demoproject/Dictionary_Assignment.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace demoproject
+{
+    internal class Dictionary_Assignment
+    {
+        static void Main()
+        {
+            Dictionary<string ,int > dict = new Dictionary<string ,int>();
+            
+                dict.Add("Apple", 75000);
+                dict.Add("vivo", 10000);
+                dict.Add("samsung", 20000);
+                dict.Add("oppo", 15000);
+                dict.Add("moto", 30000);
+            dict.Add("techno", 12000);
+            dict.Add("redme", 17000);
+            dict.Add("pro", 110000);
+            dict.Add("itel", 80000);
+            dict.Add("karban",50000);
+
+
+
+            Console.WriteLine("Display the element");
+            foreach (KeyValuePair<string,int> kvp in dict)
+            {
+                Console.WriteLine(kvp.Key); 
+            }
+            Console.ReadLine();
+            dict.Remove("Apple");
+            Console.WriteLine("cut the element");
+            foreach (KeyValuePair<string, int> kvp in dict)
+            {
+                Console.WriteLine(kvp.Key);
+            }
+            Console .ReadLine();
+            dict .Clear();
+            Console.WriteLine("clear the element");
+            foreach (KeyValuePair<string, int> kvp in dict)
+            {
+                Console.WriteLine(kvp.Key);
+            }
+
+            
+            
+        }
+    }
+}
---
+++ demoproject/Nongeneric_Assignment.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace demoproject
+{
+    internal class Nongeneric_Assignment
+    {
+        static void Main()
+        {
+            Stack st = new Stack();
+            st.Push(12);
+            st.Push(13);
+            st.Push("vikas");
+            st.Push(233);
+            st.Push('V');
+            Console.WriteLine("stack element");
+            foreach (var item in st)
+            {
+                Console.WriteLine(item);
+            }
+            st.Pop();
+            st .Pop();
+            st.Pop();
+         Console.WriteLine("Remove the element");
+            foreach (var item in st)
+            {
+                Console.WriteLine(item);
+            }
+            st .Clear();
+            Console.WriteLine("clear the element");
+            foreach (var item in st)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
+
+
+
