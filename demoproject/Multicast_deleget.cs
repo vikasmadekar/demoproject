1e29bbb6b0fe3c1e257718c6912b1c77ea4b2436
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-10-12 14:11:43 +0530

    multicastdeleget

---
+++ demoproject/Multicast_deleget.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+
+    public delegate void Alldeleget(int a ,int b);
+
+
+    class Mul
+    {
+        public void  Addition(int a,int b)
+        {
+           Console .WriteLine (a + b);
+        }
+         
+
+        public void subtraction(int a,int b)
+        {
+            Console.WriteLine(a - b);
+                
+
+            
+        }
+        public void multiplication(int a,int b)
+        {
+            Console.WriteLine(a * b);
+        }
+    }
+    internal class Multicast_deleget
+    {
+        static void Main()
+        {
+            Mul mul = new Mul();
+
+
+            Alldeleget a=new Alldeleget (mul.Addition);
+            a += mul.subtraction; 
+            a += mul.multiplication;
+            a(20, 25);
+
+        }
+    }
+}
