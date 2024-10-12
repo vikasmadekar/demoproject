2ac83c42b7fbdaaa10be9e53b1c144503b82c637
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-10-12 13:00:13 +0530

    Stacck

---
+++ demoproject/Singnalcast_deleget.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+
+    public delegate void Additiondelegate(int a,int b); //deleget declare 
+    public delegate void Subtractiondelegate(int a,int b);
+
+    class Vm  // class
+    {
+        public void Add(int a,int b)  // method 
+        {
+            Console.WriteLine(a+b);
+        }
+        public void Sub(int a,int b)
+        {
+            Console.WriteLine(a-b);
+        }
+    }
+ class Singnalcast_deleget  // main class
+    {
+        static void Main()
+        {
+            Vm v = new Vm();  //  class chaya object create kela
+            Additiondelegate a = new Additiondelegate(v.Add); // deleget mdhe main class chaya 
+                                                                             //object pass kela
+            v.Add(30,20);
+            v.Sub(40, 20);
+                
+        }
+    }
+    
+}
