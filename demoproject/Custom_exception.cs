9203b991e49d954fe03a288863b52adff05c1157
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-10-13 16:46:04 +0530

    custom exception

---
+++ demoproject/Custom_exception.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+
+
+    class Negative_Number_Exception : Exception
+    {
+        public Negative_Number_Exception(string message) : base(message)
+        {
+
+        }
+
+
+
+
+
+    } 
+    internal class Custom_exception
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("enter the number");
+                int num = int.Parse(Console.ReadLine());
+                if (num < 0)
+                {
+                    throw new Negative_Number_Exception ("num is negative number");
+
+                }
+                else
+                {
+                    Console.WriteLine(num );
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message );
+            }
+        }
+    }
+}
