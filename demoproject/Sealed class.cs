c7cd52a9ee2a0ea231ece3afa05668b248ef1de0
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-10-17 11:24:26 +0530

    sealed and static class

---
+++ demoproject/Sealed class.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+
+
+    
+{
+
+   class  Sealed1 
+    {
+       public void Sohw()
+        {
+            int m=40; int b=20;
+            int z = m + b;
+            Console.WriteLine(z);
+        }
+     
+    }
+    sealed class  Madekar : Sealed1
+    {
+           
+          public void Display()
+        {
+
+            int a = 40; int e = 20;
+            int v = a - e;
+            Console.WriteLine(e);
+        }
+
+    }
+    
+    internal class Sealed_class
+    {
+
+        static void Main()
+        {
+            Madekar m=new Madekar ();
+            m.Display();
+            m.Sohw();
+        }
+    }
+}
---
+++ demoproject/Static class.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    static class CatWorld
+    {
+        public static int NumberOfCats { get; set; }
+        public static int TotalCatNaps { get; set; }
+        static CatWorld()
+        {
+            NumberOfCats = 10;
+        }
+        public static void AddCat()
+        {
+            Console.WriteLine($"Current Cat Count Is:- {NumberOfCats}");
+            NumberOfCats++;
+            Console.WriteLine($"\nNew Cat Has Joined The Current Count Is:- {NumberOfCats}\n");
+        }
+    }
+     internal class StaticClass
+    {
+        static void Main()
+        {
+            CatWorld.AddCat(); //New Cat Joined
+            CatWorld.AddCat();//Another New cat Joined
+        }
+   
+    
+               
+        }
+      
+        
+           
+}
