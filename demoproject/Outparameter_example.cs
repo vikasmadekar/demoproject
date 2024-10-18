4364da71f329fe50b98fdb059d49cd37c86a0098
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-10-18 15:43:18 +0530

    Add  a class

---
+++ demoproject/Outparameter_example.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    internal class Outparameter_example
+    {
+        static void Main()
+        {
+            int a;
+            string str;
+           
+            InitializeValue(out  int num,out string text);
+
+            Console.WriteLine($"Number :{num}");
+            Console.WriteLine($"String : {text}");
+
+           static void  InitializeValue(out  int num,out string text)
+            {
+                num = 34;
+                text = "VIKAS MADEKAR";
+            }
+
+
+
+        }
+
+    }
+}
---
+++ demoproject/Ref _Example.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    
+    
+    
+   public  class Ref_Example
+    {
+        static void incrementvalueNum(ref int M)
+        {
+            M++;
+        }
+
+
+        static void Main()
+        {
+            int V  = 10;
+            Console.WriteLine($"Befour incrementvalue of v is :-{V}");
+            incrementvalueNum(ref V);
+            Console.WriteLine($"After incrementvale of v  is :{V}");
+
+        }
+
+
+    }
+}
